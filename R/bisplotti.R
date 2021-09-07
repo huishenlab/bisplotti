8cc9c0aa95330b2d7d19b3d7d452eebd37342f0c
Ben Johnson <ben.johnson4343@gmail.com>
2021-09-07 10:09:31 -0400

    initial moving of epistate.R to bisplotti from biscuiteer

--- R/bisplotti.R
+++ R/bisplotti.R
@@ -1,6 +1,6 @@
 # Here's a good place to put your top-level package documentation
 
-.onAttach <- function (lib, pkgname="bisplotly") {
+.onAttach <- function (lib, pkgname="bisplotti") {
     ## Put stuff here you want to run when your package is loaded
     invisible()
 }
