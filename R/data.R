e29ce5194ad53b664e31f6b256ee5eb4e539a630
Jacob Morrison <Jacob.Morrison@vai.org>
2021-10-13 14:10:54 -0400

    Add data and extdata files

---
+++ R/data.R
@@ -0,0 +1,47 @@
+#' hg38 seqlengths as a GRanges object
+#' 
+#' This object was generated using the BSgenome.Hsapiens.UCSC.hg38 package.
+#' The script used for this object is found in the inst/scripts directory
+#' 
+#' @name hg38.gr
+#' @docType data
+#' @author Benjamin K Johnson \email{ben.johnson@vai.org}
+#' @keywords data
+#' @usage data(hg38.gr, package = "bisplotti")
+NULL
+
+#' hg19 seqlengths as a GRanges object
+#' 
+#' This object was generated using the Homo.sapiens package.
+#' The script used for this object is found in the inst/scripts directory
+#' 
+#' @name hg19.gr
+#' @docType data
+#' @author Benjamin K Johnson \email{ben.johnson@vai.org}
+#' @keywords data
+#' @usage data(hg19.gr, package = "bisplotti")
+NULL
+
+#' mm10 seqlengths as a GRanges object
+#' 
+#' This object was generated using the Mus.musculus package.
+#' The script used for this object is found in the inst/scripts directory
+#' 
+#' @name mm10.gr
+#' @docType data
+#' @author Benjamin K Johnson \email{ben.johnson@vai.org}
+#' @keywords data
+#' @usage data(mm10.gr, package = "bisplotti")
+NULL
+
+#' mm9 seqlengths as a GRanges object
+#' 
+#' This object was generated using the BSgenome.Mmusculus.UCSC.mm9 package.
+#' The script used for this object is found in the inst/scripts directory
+#' 
+#' @name mm9.gr
+#' @docType data
+#' @author Benjamin K Johnson \email{ben.johnson@vai.org}
+#' @keywords data
+#' @usage data(mm9.gr, package = "bisplotti")
+NULL
